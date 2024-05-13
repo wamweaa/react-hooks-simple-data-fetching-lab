62ec54bb5ba744e32ca816350ebae2a62b68e4ea
wamwea <patokinya12@gmail.com>
2024-05-13 08:38:51 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+
+import React ,{useEffect ,useState}from "react";
+function App (){
+const [image ,setImage]=useState(null)
+useEffect(()=>{
+    fetch(" https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+      .then((data) => {
+        setImage(data.message)});
+},[])
+
+if (!image) return <p>Loading...</p>;
+    return(
+        <div>
+
+<img src={image} alt="A Random Dog"/>
+        </div>
+    )
+}
+export default App;
